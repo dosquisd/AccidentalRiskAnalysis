235260e3bd2544e83cf5b1d0bfa2b74fcc7e2aa8
JuanD <dosquisd123@gmail.com>
2025-11-01 19:20:35 -0500

    feat: add skeleton file for boxplots

---
+++ dashboard/boxplots.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def boxplots_index(title: str = "Box Plots") -> None:
+    st.markdown(f"# {title}")
+    st.warning("""
+        TODO: :red[Write about the box plots that will be shown here. Explain what they represent,
+        the data source, and any relevant information for the user to understand the visualizations.]
+    """)
